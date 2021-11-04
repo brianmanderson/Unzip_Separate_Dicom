ab37c36f4f7090023822c74f90fbf06e6f4f23ac
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2021-11-04 15:02:07 -0700

    rewrite to have a list of images and a new series instance UID

--- Main.py
+++ Main.py
@@ -12,8 +12,8 @@ def read_dicom_header(path, file, dicom_dictionary):
     series_description = ds.SeriesDescription
     for item in [series_description]:
         if item not in dicom_dictionary:
-            dicom_dictionary[item] = []
-        dicom_dictionary[item].append(file)
+            dicom_dictionary[item] = {'Images': [], 'NewFrameOfRef': None}
+        dicom_dictionary[item]['Images'].append(file)
 
 
 def dicom_reader_worker(A):
@@ -52,32 +52,34 @@ def rename_folder(base_path, dicom_path):
     return dicom_path
 
 
-def separate_into_series(dicom_path):
+def separate_into_folders(dicom_path, dicom_dictionary):
+    for series_description in dicom_dictionary:
+        out_path = os.path.join(dicom_path, series_description).replace(':', '').replace('>', '')
+        if not os.path.exists(out_path):
+            os.makedirs(out_path)
+        for dicom_file in dicom_dictionary[series_description]['Images']:
+            os.rename(os.path.join(dicom_path, dicom_file), os.path.join(out_path, dicom_file))
+        fid = open(os.path.join(out_path, 'Separated.txt'), 'w+')
+        fid.close()
+    return None
+
+
+def create_dicom_dictionary(dicom_path, dicom_dictionary):
     dicom_files = [i for i in os.listdir(dicom_path) if i.endswith('.dcm')]
-    if dicom_files:
-        dicom_dictionary = dict()
-        thread_count = int(cpu_count()*.5)
-        q = Queue(maxsize=int(thread_count))
-        A = (q, dicom_dictionary)
-        threads = []
-        for worker in range(thread_count):
-            t = Thread(target=dicom_reader_worker, args=(A,))
-            t.start()
-            threads.append(t)
-        for dicom_file in dicom_files:
-            q.put([dicom_path, dicom_file])
-        for i in range(thread_count):
-            q.put(None)
-        for t in threads:
-            t.join()
-        for series_description in dicom_dictionary:
-            out_path = os.path.join(dicom_path, series_description).replace(':', '').replace('>', '')
-            if not os.path.exists(out_path):
-                os.makedirs(out_path)
-            for dicom_file in dicom_dictionary[series_description]:
-                os.rename(os.path.join(dicom_path, dicom_file), os.path.join(out_path, dicom_file))
-            fid = open(os.path.join(out_path, 'Separated.txt'), 'w+')
-            fid.close()
+    thread_count = int(cpu_count()*.5)
+    q = Queue(maxsize=int(thread_count))
+    A = (q, dicom_dictionary)
+    threads = []
+    for worker in range(thread_count):
+        t = Thread(target=dicom_reader_worker, args=(A,))
+        t.start()
+        threads.append(t)
+    for dicom_file in dicom_files:
+        q.put([dicom_path, dicom_file])
+    for i in range(thread_count):
+        q.put(None)
+    for t in threads:
+        t.join()
     return None
 
 
@@ -88,7 +90,7 @@ def main():
         time.sleep(3)
         zip_files = [i for i in os.listdir(path) if i.endswith('.zip')]
         """
-        This will unzip, rename the folder, and separate them into unique folders
+        This will unzip and rename the folder
         """
         for zip_file in zip_files:
             print('Files found...')
@@ -99,7 +101,6 @@ def main():
                 unzip_file(file_path=os.path.join(path, zip_file), output_path=output_path)
                 os.remove(os.path.join(path, zip_file))  # Delete zipped file
             new_path = rename_folder(base_path=path, dicom_path=output_path)
-            # separate_into_series(new_path)  # Separate into their respective exams
         for root, folders, files in os.walk(path):
             dicom_files = [os.path.join(root, i) for i in files if i.endswith('.dcm')]
             if dicom_files:
@@ -108,13 +109,14 @@ def main():
                 while len(os.listdir(root)) != len(files):
                     time.sleep(5)
                     files = os.listdir(root)
-                dicom_files = [os.path.join(root, i) for i in files if i.endswith('.dcm')]
                 if 'NewFrameOfRef.txt' in files:
                     continue
-                if 'Separated.txt' not in files:
-                    separate_into_series(root)  # Separate into their respective exams
-                    continue
-                new_frame_of_ref = pydicom.uid.generate_uid()
+                dicom_dictionary = dict()
+                create_dicom_dictionary(dicom_path=root, dicom_dictionary=dicom_dictionary)
+                for description_key in dicom_dictionary:
+                    new_frame_of_ref = pydicom.uid.generate_uid()
+                    dicom_dictionary[description_key]
+
                 for dicom_file in dicom_files:
                     ds = pydicom.read_file(dicom_file)
                     ds.FrameOfReferenceUID = new_frame_of_ref
