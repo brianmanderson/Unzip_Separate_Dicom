1741ea0eaf848beaf1ed7ec6c8b8bdf6c199951c
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2021-11-04 14:56:40 -0700

    run all of this in parallel

--- Main.py
+++ Main.py
@@ -2,6 +2,9 @@ import os
 import pydicom
 import time
 import zipfile, os, tarfile
+from threading import Thread
+from multiprocessing import cpu_count
+from queue import *
 
 
 def read_dicom_header(path, file, dicom_dictionary):
@@ -13,14 +16,15 @@ def read_dicom_header(path, file, dicom_dictionary):
         dicom_dictionary[item].append(file)
 
 
-def contour_worker(A):
+def dicom_reader_worker(A):
     q, dicom_dictionary = A
     while True:
-        file = q.get()
-        if file is None:
+        item = q.get()
+        if item is None:
             break
         else:
-            read_dicom_header(file, dicom_dictionary)
+            dicom_path, dicom_file = item
+            read_dicom_header(path=dicom_path, file=dicom_file, dicom_dictionary=dicom_dictionary)
         q.task_done()
 
 
@@ -52,23 +56,40 @@ def separate_into_series(dicom_path):
     dicom_files = [i for i in os.listdir(dicom_path) if i.endswith('.dcm')]
     if dicom_files:
         dicom_dictionary = dict()
+        thread_count = int(cpu_count()*.5)
+        q = Queue(maxsize=int(thread_count))
+        A = (q, dicom_dictionary)
+        threads = []
+        for worker in range(thread_count):
+            t = Thread(target=dicom_reader_worker, args=(A,))
+            t.start()
+            threads.append(t)
         for dicom_file in dicom_files:
-            read_dicom_header(path=dicom_path, file=dicom_file, dicom_dictionary=dicom_dictionary)
+            q.put([dicom_path, dicom_file])
+        for i in range(thread_count):
+            q.put(None)
+        for t in threads:
+            t.join()
         for series_description in dicom_dictionary:
             out_path = os.path.join(dicom_path, series_description).replace(':', '').replace('>', '')
             if not os.path.exists(out_path):
                 os.makedirs(out_path)
             for dicom_file in dicom_dictionary[series_description]:
                 os.rename(os.path.join(dicom_path, dicom_file), os.path.join(out_path, dicom_file))
+            fid = open(os.path.join(out_path, 'Separated.txt'), 'w+')
+            fid.close()
     return None
 
 
 def main():
-    path = r'\\ucsdhc-varis2\radonc$\00plans\LifeImage Exports'
+    path = r'\\ucsdhc-varis2\radonc$\00plans\Separate'
     while True:
         print('Waiting...')
         time.sleep(3)
         zip_files = [i for i in os.listdir(path) if i.endswith('.zip')]
+        """
+        This will unzip, rename the folder, and separate them into unique folders
+        """
         for zip_file in zip_files:
             print('Files found...')
             time.sleep(10)
@@ -78,7 +99,28 @@ def main():
                 unzip_file(file_path=os.path.join(path, zip_file), output_path=output_path)
                 os.remove(os.path.join(path, zip_file))  # Delete zipped file
             new_path = rename_folder(base_path=path, dicom_path=output_path)
-            # separate_into_series(new_path)  # Separate into their respective exams
+            separate_into_series(new_path)  # Separate into their respective exams
+        for root, folders, files in os.walk(path):
+            dicom_files = [os.path.join(root, i) for i in files if i.endswith('.dcm')]
+            if dicom_files:
+                # Check to make sure all of the files are transferred over
+                time.sleep(5)
+                while len(os.listdir(root)) != len(files):
+                    time.sleep(5)
+                    files = os.listdir(root)
+                dicom_files = [os.path.join(root, i) for i in files if i.endswith('.dcm')]
+                if 'NewFrameOfRef.txt' in files:
+                    continue
+                if 'Separated.txt' not in files:
+                    separate_into_series(root)  # Separate into their respective exams
+                    continue
+                new_frame_of_ref = pydicom.uid.generate_uid()
+                for dicom_file in dicom_files:
+                    ds = pydicom.read_file(dicom_file)
+                    ds.FrameOfReferenceUID = new_frame_of_ref
+                    pydicom.write_file(filename=dicom_file, dataset=ds)
+                fid = open(os.path.join(root, 'NewFrameOfRef.txt'), 'w+')
+                fid.close()
 
 
 if __name__ == '__main__':
